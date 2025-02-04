1ddf7395205007be3e5d2cf37e7105d1018016fd
gurez <boxbrothersocial@gmail.com>
2025-02-04 17:21:53 +0530

    first commit

--- src/module/layout/sidebar/nav-user.tsx
+++ src/module/layout/sidebar/nav-user.tsx
@@ -29,18 +29,17 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar"
+import { signOut, useSession } from "next-auth/react"
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string
-    email: string
-    avatar: string
-  }
-}) {
+export function NavUser() {
   const { isMobile } = useSidebar()
-
+  const { data: session } = useSession();
+  if (!session) {
+    return null
+  }
+  const logOutHandler = async () => {
+    await signOut({ callbackUrl: "/sign-in" });
+  }
   return (
     <SidebarMenu>
       <SidebarMenuItem>
@@ -50,13 +49,22 @@ export function NavUser({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              <Avatar className="h-8 w-8">
+                <AvatarImage
+                  src={session.user.image ?? ""}
+                  alt={session.user.name ?? "User"}
+                />
+                <AvatarFallback>
+                  {session.user.name
+                    ?.split(" ")
+                    .map((n) => n[0])
+                    .join("") ?? "U"}
+                </AvatarFallback>
               </Avatar>
+
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
+                <span className="truncate font-semibold">{session.user.name}</span>
+                <span className="truncate text-xs">{session.user.email}</span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -69,13 +77,21 @@ export function NavUser({
           >
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                <Avatar className="h-8 w-8">
+                  <AvatarImage
+                    src={session.user.image ?? ""}
+                    alt={session.user.name ?? "User"}
+                  />
+                  <AvatarFallback>
+                    {session.user.name
+                      ?.split(" ")
+                      .map((n) => n[0])
+                      .join("") ?? "U"}
+                  </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                  <span className="truncate font-semibold">{session.user.name}</span>
+                  <span className="truncate text-xs">{session.user.email}</span>
                 </div>
               </div>
             </DropdownMenuLabel>
@@ -102,7 +118,7 @@ export function NavUser({
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
+            <DropdownMenuItem onClick={logOutHandler}>
               <LogOut />
               Log out
             </DropdownMenuItem>
